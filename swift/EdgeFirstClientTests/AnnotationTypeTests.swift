21e3f85d68eb813b9bb13cf7fdc68a0fc8d4f32e
Sébastien Taylor <sebastien@au-zone.com>
2026-03-26 08:58:37 -0600

    Fix duplicate Polygon typealias across Swift test files
    Signed-off-by: Sébastien Taylor <sebastien@au-zone.com>

--- swift/EdgeFirstClientTests/AnnotationTypeTests.swift
+++ swift/EdgeFirstClientTests/AnnotationTypeTests.swift
@@ -10,9 +10,6 @@ import XCTest
 
 @testable import EdgeFirstClient
 
-// Disambiguate from ApplicationServices.Polygon (macOS system type)
-typealias Polygon = EdgeFirstClient.Polygon
-
 final class AnnotationTypeTests: XCTestCase {
 
   // MARK: - Box2d Tests
